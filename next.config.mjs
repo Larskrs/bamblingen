8769cbeff08594045a4c98c5e9ff17ea7be9f76f
Larskrs <lkssyv@gmail.com>
2024-12-06 11:06:48 +0100

    Smile

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'bamblingen.no'
+                hostname: 'www.bamblingen.no',
             }
         ]
     }
