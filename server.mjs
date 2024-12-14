969b8a513acd8963604c7759a08939aafc4d6d50
Larskrs <lkssyv@gmail.com>
2024-12-14 21:59:55 +0100

    More logs in startup

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import { createServer } from 'http'
 import { parse } from 'url'
 import next from 'next'
+import { logger } from './logger'
  
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
@@ -18,4 +19,7 @@ app.prepare().then(() => {
       dev ? 'development' : process.env.NODE_ENV
     }`
   )
+
+  logger.info("Server started")
+  logger.info(`${process.env.AUTH_GOOGLE_ID}, ${process.env.AUTH_GOOGLE_SECRET}, ${process.env.AUTH_SECRET}, ${process.env.AUTH_URL}`)
 })
