90a5375416b1813149bf2f10433aeb4d458db533
Larskrs <lkssyv@gmail.com>
2024-12-14 22:09:32 +0100

    Hehe

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import { createServer } from 'http'
 import { parse } from 'url'
 import next from 'next'
-import { logger } from './logger'
+import { logger } from './logger.mjs'
  
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
