b95d5bd67166c0e4de81402900b9274a10598687
Larskrs <lkssyv@gmail.com>
2024-12-18 09:08:23 +0100

    Data displa

--- src/app/(main)/news/page.jsx
+++ src/app/(main)/news/page.jsx
@@ -6,13 +6,22 @@ import useFetch from "@/hooks/useFetch";
 
 export default function News () {
 
-    const { data, error, loading } = useFetch("/api/v1/articles")
+    const { data, error, loading } = useFetch("/api/v1/articles?showAuthors=true")
 
     console.log("Fetch: ", data, error, loading)
     return (
         <>
             { loading && <h1>Loading...</h1> }
             { error && <h1>{error}</h1> }
+
+            {data.map((article) => {
+                return (<div key={article.id}>
+                    <h2>{article.title}</h2>
+                    {article.authors.map((author) => {
+                        return <p key={author.id}>{author.name}</p>
+                    })}
+                </div>)
+            })}
         </>
     )
 }
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -12,15 +12,11 @@ const useFetch = (url) => {
       setLoading(true);
       try {
         const response = await axios.get(url);
-        console.log(response)
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
         const result = await response.json();
         setData(result);
-      } catch (err) {
+    } catch (err) {
         setError(err.message);
-      } finally {
+    } finally {
         setLoading(false);
       }
     };
