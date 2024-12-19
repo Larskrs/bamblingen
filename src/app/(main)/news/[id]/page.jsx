ca66135083c0447819efe0bd48fd3ecfce8f051c
Larskrs <lkssyv@gmail.com>
2024-12-19 22:58:07 +0100

    Added Article Retreivela

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -5,42 +5,20 @@ import { formatRelativeDate } from "@/lib/timeLib";
 import axios, { AxiosRequestConfig } from 'axios';
 import { notFound } from "next/navigation";
 import { logger } from "logger.mjs";
+import { GetArticle } from "@/lib/articleLib";
 
 export default async function NewsArticlePage ({ params }) {
 
     const parm = await params
     const id = parm.id
-	const host = "http://localhost:3000"
-    const url = `${host}/api/v1/articles/${id}`
-
-    const axioss = axios.create({
-        baseURL: `http://localhost:3000`,
-        headers: {
-            'Accept': 'application/json',
-        },
-    });
-
-    console.log(url)
-
-    let data;
-    try {
-        const response = await axioss(url); // Optional: Add revalidation for caching
-        if (!response.statusText == "ok") {
-            console.error("Failed to fetch article:", response.status, response.statusText);
-            // return notFound();
-        }
-        data = await response.data
-        console.log(data)
-    } catch (error) {
-        console.error("Error fetching article:", error);
-        // return notFound();
-    }
+	
+    const article = await GetArticle(id)
 
     return (
         <div className={styles.c}>
-            <h1>{data.title}</h1>
+            <h1>{article.title}</h1>
+            <p>Sist oppdatert {formatRelativeDate(new Date(article.createdAt))}</p>
             <p>Article for {await id}</p>
-			<p>{url}</p>
         </div>
     )
 }
