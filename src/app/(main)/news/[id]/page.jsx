175728810f7cac0b6621205582c2816225dfd72b
Larskrs <lkssyv@gmail.com>
2024-12-19 01:14:27 +0100

    DFans

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -4,23 +4,6 @@ import { formatRelativeDate } from "@/lib/timeLib";
 import axios from "axios";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata ({ params }) {
-
-    const parm = await params
-    const id = parm.id
-
-    const response = await axios.get(`${process.env.PUBLIC_NEXT_URL}/api/v1/articles/${id}`)
-    const data = response.data
-
-    return {
-        twitter: {
-            card: 'summary_large_image',
-            title: data.title,
-            description: `Beskrivelse for artikkel`,
-            creator: `${data.authors.map((a) => a.name)}`,
-        },
-    }
-}
 export default async function NewsArticlePage ({ params }) {
 
     const parm = await params
