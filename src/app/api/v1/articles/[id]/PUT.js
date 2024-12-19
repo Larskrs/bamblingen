37fdc0382decaa44dc43d3eb2be455759c3fba06
Larskrs <lkssyv@gmail.com>
2024-12-19 23:52:23 +0100

    HAHA

---
+++ src/app/api/v1/articles/[id]/PUT.js
@@ -0,0 +1,41 @@
+import { MAX_PER_PAGE } from "@/lib/articleLib";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export default async function GET(req, params) {
+
+    const parm = await params
+    const id = parm.id
+
+    try {
+        // Dynamically build the query based on optional parameters
+        const query = {
+            data: {
+                categories: {
+                    connectOrCreate: [
+                        {
+                            where: { slug: "technology"},
+                            create: { name: "Technology", slug: "technology"}
+                        }
+                    ]
+                }
+            },
+            where: {
+                id: id
+            }, // Initialize an empty `where` object
+        };
+
+        // Fetch data from the database with the constructed query
+        const data = await db.article.update(query);
+
+        return NextResponse.json( data );
+    } catch (err) {
+        return NextResponse.json(
+            {
+                message: "Could not retrieve articles due to an error",
+                error: err.message || err,
+            },
+            { status: 500 }
+        );
+    }
+}
--- src/app/api/v1/articles/[id]/route.js
+++ src/app/api/v1/articles/[id]/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 import GetRequest from "./GET"
+import PutRequest from "./PUT"
 
 export async function GET (req, {params}) {
     return await GetRequest(req, params)
+}
+
+export async function PUT (req, {params}) {
+    return await PutRequest(req, params)
 }
