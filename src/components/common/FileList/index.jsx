77e410c0341201da2882a7967a06414e52a846ce
Larskrs <lkssyv@gmail.com>
2024-12-31 17:21:01 +0100

    Ikkje flir

--- src/components/common/FileList/index.jsx
+++ src/components/common/FileList/index.jsx
@@ -2,21 +2,22 @@
 import Link from "next/link"
 import styles from "./style.module.css"
 import Image from "next/image"
+import { useRouter } from 'next/navigation'
 
 export function FileList ({files}) {
+
+    const router = useRouter()
 
     if (!files) {
         return <>Loading...</>
     }
 
-    console.log(files)
-
     return <div className={styles.c}>
         {files.map((f, i) => {
                 return (
-                    <Link style={{animationDelay: `${i*50}ms`}} className={styles.file} href={`/api/v1/files?fileId=${f.id}`} key={f.id}>
+                    <div style={{animationDelay: `${i*50}ms`}} className={styles.file} onClick={() => {router.push(`/api/v1/files?fileId=${f.id}`)}} key={f.id}>
                         <Image alt={f.name} width={256} height={256} src={`/api/v1/files?fileId=${f.id}`} />
-                    </Link>
+                    </div>
                 )
             })}
     </div>
