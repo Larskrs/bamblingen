fc6dc138cea0615767d18be4917c32ffe1ec2704
Larskrs <lkssyv@gmail.com>
2025-01-12 13:09:31 +0100

    Copyrighting

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -118,7 +118,7 @@ export default function TestArticle () {
                     },
                     {
                         type: "grid",
-                        layout: "G_1",
+                        layout: "G_FULL",
                         priority: "normal",
                         items: [
                             {
--- src/components/frontPage/rows/header/index.jsx
+++ src/components/frontPage/rows/header/index.jsx
@@ -10,7 +10,7 @@ export default function Header ({items}) {
 function Render ({image, context, priorityPrefix=false, title}) {
     return (
         <div className={styles.header}>
-            <Image alt="Header Image" src={image} className={styles.img} width={2000} height={1000} />
+            <Image alt="Header Image" src={image} className={styles.img} width={2000} height={1000} />*
             <div className={styles.bottom}>
                 <div className={styles.details}>
                     <h1>
