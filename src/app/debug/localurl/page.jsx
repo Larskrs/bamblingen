8f9e9b1c58a1dca3ed72acb2867cc842c4b93572
Larskrs <lkssyv@gmail.com>
2025-02-14 16:56:38 +0100

    Debug Page (1)

--- src/app/debug/localurl/page.jsx
+++ src/app/debug/localurl/page.jsx
@@ -5,14 +5,15 @@ import { LocalizeURL } from "@/lib/mediaLib";
 import Image from "next/image";
 import { useState } from "react";
 
-export default function page ({}) {
+export default function LocalUrlDebugPage ({}) {
 
     const [url, setUrl] = useState("https://bamblingen.no/api/v1/files?fileId=20250105-b217d32b83c1660f3835d7b399c2794fe93e6e34c22599d6")
 
     return (
         <div style={{padding: "2rem"}}>
             <Image src={LocalizeURL(url)} width={720} height={512} />
-            <p>{LocalizeURL(url)}</p>
+            <p>URL: {LocalizeURL(url)}</p>
+            <p>NEXT_PUBLIC_URL: {process.env.NEXT_PUBLIC_URL}</p>
         </div>
     );
 }
