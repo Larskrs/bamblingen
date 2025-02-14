92d23b6d3e9351badd9ef8931d592da8b442e240
Larskrs <lkssyv@gmail.com>
2025-02-14 17:15:16 +0100

    Test multiple URL's with debug

--- src/components/frontPage/rows/grid/items/article/index.jsx
+++ src/components/frontPage/rows/grid/items/article/index.jsx
@@ -2,14 +2,15 @@ import Image from "next/image"
 import styles from "./style.module.css"
 import Pulse from "@/components/details/pulse"
 import ZoomInWrapper from "@/components/wrappers/ZoomInWrapper"
+import { LocalizeURL } from "@/lib/mediaLib"
 
 export default function FullGrid ({id, context, title, image, priority, priorityPrefix=false, fullImage, fontSize=2}) {
     return (
         <>
             <section key={id} className={`${styles.section} ${styles[priority]}`}>
-                    <Image  alt="Image" src={image} className={`${styles.img} ${fullImage && styles.fullImage}`} quality={100} width={1000} height={500} />
+                    <Image alt="Image" src={LocalizeURL(image)} className={`${styles.img} ${fullImage && styles.fullImage}`} quality={100} width={1000} height={500} />
                     <div className={`${styles.details} ${fullImage && styles.overlay}`}>
-                        <p>{context}</p>
+                        <p>{LocalizeURL(image)}</p>
                         <h1 style={{fontSize: `${fontSize}rem`}}>
                             {priorityPrefix && <span className={styles.latest}>
                                 <Pulse />
